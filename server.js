f00b65018f8c6806e20a7cb31f20f077476f556c
andreyvarlamov <varlamov.and@gmail.com>
2020-05-05 17:52:31 -0700

    Add mongoose connection, env file management

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const db = process.env.MONGO_URI;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(() => console.log("Server started on port " + port + "..."));
